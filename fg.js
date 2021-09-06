2e5e6d91a78b5dd9af5471bba04edead48e7b52c
Egor <egor18mochalov@yandex.ru>
2021-09-06 16:55:51 +0300

    sdsdsds

--- fg.js
+++
@@ -1,6 +0,0 @@
-let smena = document.querySelector('.butto');
-let sm = document.querySelector('.osnova');
-smena.onclick=function(){
-  sm.classList.toggle('osnova');
-  sm.classList.toggle('none');
-}
